ea93ad224cf1a23dd137db23111456de5d70fbd9
Ethan He <tashaxing123@163.com>
2016-07-18 10:48:45 +0800

    add

--- game_model.cpp
+++ game_model.cpp
@@ -1,6 +1,40 @@
+#include <string.h>
+#include <stdlib.h>
 #include "game_model.h"
 // 游戏逻辑模型，与界面分离
-GameModel::GameModel()
+GameModel::GameModel() :
+    gameStatus(PLAYING),
+    gameLevel(BASIC)
 {
 
+}
+
+GameModel::~GameModel()
+{
+    if(gameMap)
+    {
+        free(gameMap);
+        gameMap = NULL;
+    }
+}
+
+void GameModel::startGame()
+{
+    // 用C的方式初始化数组，怀旧一下~
+    gameMap = (int *)malloc(sizeof(int) * MAX_ROW * MAX_COL);
+    memset(gameMap, 0, MAX_ROW * MAX_COL);
+    // 填充方块标号
+
+    // 打乱方块
+
+}
+
+bool GameModel::isCanLink(int srcX, int srcY, int dstX, int dstY)
+{
+    return false;
+}
+
+bool GameModel::linkTwoTiles(int srcX, int srcY, int dstX, int dstY)
+{
+    return false;
 }
--- game_model.h
+++ game_model.h
@@ -1,11 +1,48 @@
 #ifndef GAMEMODEL_H
 #define GAMEMODEL_H
 
+// ------------ 全局变量 ------------ //
+// 最大行和列数
+static const int MAX_ROW = 20;
+static const int MAX_COL = 20;
+// 游戏可玩图片数量
+static const int MAX_ICON = 25;
+// 游戏状态
+enum GameStatus
+{
+    PLAYING,
+    PAUSE,
+    WIN,
+    OVER
+};
+// 游戏难度
+enum GameLevel
+{
+    BASIC,
+    MEDIUM,
+    HARD
+};
+
+// -------------------------------- //
 
 class GameModel
 {
 public:
     GameModel();
+    virtual ~GameModel();
+public:
+    void startGame(); // 开始游戏
+    int *gatGameMap();    // 获得地图
+    GameStatus checkGameStatus(); // 判断获得游戏状态，是否输赢
+    bool linkTwoTiles(int srcX, int srcY, int dstX,int dstY); // 连接起点和终点方块，连接是否成功
+private:
+    // 游戏地图，存储方块，0表示消失，1-其他数字表示图片标号
+    int *gameMap;
+    // 游戏状态和难度
+    GameStatus gameStatus;
+    GameLevel gameLevel;
+    // 判断起点到终点的方块是否可以连接消除
+    bool isCanLink(int srcX, int srcY, int dstX, int dstY);
 };
 
 #endif // GAMEMODEL_H
