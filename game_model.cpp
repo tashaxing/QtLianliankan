f36c791fcad6919688414002a274e1857b9f7a2a
Ethan He <tashaxing123@163.com>
2016-12-13 21:13:59 +0800

    add

--- game_model.cpp
+++ game_model.cpp
@@ -18,6 +18,7 @@ GameModel::~GameModel()
     if(gameMap)
     {
         free(gameMap);
+        free(hintArray);
         gameMap = NULL;
     }
 }
@@ -27,6 +28,10 @@ void GameModel::startGame()
     // 用C的方式初始化数组，怀旧一下~
     gameMap = (int *)malloc(sizeof(int) * MAX_ROW * MAX_COL);
     memset(gameMap, 0, MAX_ROW * MAX_COL);
+
+    hintArray = (int *)malloc(sizeof(int) * 4);
+    memset(hintArray, -1, 4);
+
 
     // 填充方块标号
     int iconID = 0;
@@ -53,6 +58,36 @@ int *GameModel::getGameMap()
     return gameMap;
 }
 
+bool GameModel::isFrozen()
+{
+    // 暴力法，所有方块两两判断是否可以连接
+    // 每次消除后做一次判断
+    // 其实在这个过程中记录提示
+
+    for (int i = 0; i < MAX_ROW * MAX_COL - 1; i++)
+        for( int j = i + 1; j < MAX_ROW * MAX_COL; j++)
+        {
+            int srcX = i % MAX_COL;
+            int srcY = i / MAX_COL;
+            int dstX = j % MAX_COL;
+            int dstY = j / MAX_COL;
+
+            // 只要能找到可以连接的就不为僵局
+            if (isCanLink(srcX, srcY, dstX, dstY))
+            {
+                // 记录hint
+                hintArray[0] = srcX;
+                hintArray[1] = srcY;
+                hintArray[2] = dstX;
+                hintArray[3] = dstY;
+
+                return false;
+            }
+        }
+
+    return true;
+}
+
 // 最重要的判断连接算法
 bool GameModel::isCanLink(int srcX, int srcY, int dstX, int dstY)
 {
@@ -60,8 +95,26 @@ bool GameModel::isCanLink(int srcX, int srcY, int dstX, int dstY)
     // x表示横向索引，y表示纵向索引，从0开始
 
     // 情况1：横向或者竖向可以直线连通
+    if (srcX == dstX)
+    {
+        if (srcY > dstY)
+            std::swap(srcY, dstY);
+        for (int y = srcY + 1; y < dstY; y++)
+            if (gameMap[MAX_COL * y + srcX])
+                return false;
+    }
+    if (srcY == dstY)
+    {
+        if (srcX > dstX)
+            std::swap(srcX, dstX);
+        for (int x = srcX + 1; x < dstX; x++)
+            if (gameMap[MAX_COL * srcY + x])
+                return false;
+    }
 
     // 情况2：一次拐弯可以连通
+
+
 
     // 情况3：两次拐弯可以连通
 
@@ -72,6 +125,7 @@ bool GameModel::isCanLink(int srcX, int srcY, int dstX, int dstY)
     return false;
 }
 
+// 点击方块进行连接操作
 bool GameModel::linkTwoTiles(int srcX, int srcY, int dstX, int dstY)
 {
     // 成功连接就返回true否则false用于GUI里面判断
